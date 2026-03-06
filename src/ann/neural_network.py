58becadb916bd1d7d6cae094f046fbe16c554174
bhavnashri14 <bhavnashri.a@gmail.com>
2026-03-06 12:00:09 +0000

    update26

--- src/ann/neural_network.py
+++ src/ann/neural_network.py
@@ -131,8 +131,8 @@ class NeuralNetwork:
         grad_b.insert(0, self.layers[-1].grad_b)
 
         for i in reversed(range(len(self.layers))):
-          delta = self.layers[i].backward(delta)      # compute grad_W and grad_b
-          delta = self.activations[i].backward(delta) # apply activation derivative
+          delta = self.activations[i].backward(delta)      # compute grad_W and grad_b
+          delta = self.layers[i].backward(delta) # apply activation derivative
           grad_W.insert(0, self.layers[i].grad_W)
           grad_b.insert(0, self.layers[i].grad_b)
 
